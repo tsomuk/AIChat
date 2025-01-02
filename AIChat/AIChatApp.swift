a6d24560730eac966cf12806998415ebb9b7bd14
Nikita Tsomuk <123371636+tsomuk@users.noreply.github.com>
2025-01-02 09:51:56 +0200

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Nikita Tsomuk on 02.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatTests/AIChatTests.swift
@@ -0,0 +1,16 @@
+//
+//  AIChatTests.swift
+//  AIChatTests
+//
+//  Created by Nikita Tsomuk on 02.01.2025.
+//
+
+import Testing
+
+struct AIChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
