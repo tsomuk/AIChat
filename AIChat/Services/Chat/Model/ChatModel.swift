00f4bb808387dac94f1d00e649dac9145db1bf12
Nikita Tsomuk <123371636+tsomuk@users.noreply.github.com>
2025-01-05 07:52:54 +0200

    [Feature] Add ChatModel, ChatMessageModel

---
+++ AIChat/Services/Chat/Model/ChatModel.swift
@@ -0,0 +1,30 @@
+//
+//  ChatModel.swift
+//  AIChat
+//
+//  Created by Nikita Tsomuk on 05.01.2025.
+//
+
+import Foundation
+
+struct ChatModel: Identifiable {
+    let id: String
+    let userId: String
+    let avatarId: String
+    let dateCreated: Date
+    let dateModified: Date
+        
+    static var mock: ChatModel {
+        mocks[0]
+    }
+    
+    static var mocks: [ChatModel] {
+        let now = Date()
+        return [
+            ChatModel(id: "mock_chat_1", userId: "user1", avatarId: "avatar1", dateCreated: now, dateModified: now),
+            ChatModel(id: "mock_chat_2", userId: "user2", avatarId: "avatar2", dateCreated: now.addingTimeInterval(hours: -1), dateModified: now.addingTimeInterval(minutes: -30)),
+            ChatModel(id: "mock_chat_3", userId: "user3", avatarId: "avatar3", dateCreated: now.addingTimeInterval(hours: -2), dateModified: now.addingTimeInterval(hours: -1)),
+            ChatModel(id: "mock_chat_4", userId: "user4", avatarId: "avatar4", dateCreated: now.addingTimeInterval(days: -1), dateModified: now.addingTimeInterval(hours: -10))
+        ]
+    }
+}
